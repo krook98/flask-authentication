0f54102c43abcff3d1fbaaadcac253ee801064f2
krook98 <sokalski.jakub98@gmail.com>
2022-04-07 16:27:28 +0200

    Passing authentication status to templates

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from sqlalchemy.exc import SQLAlchemyError, IntegrityError
-
+from sqlalchemy.exc import IntegrityError
 
 
 app = Flask(__name__)
@@ -37,7 +36,7 @@ class User(UserMixin, db.Model):
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -62,7 +61,7 @@ def register():
             flash('User already exists. Please log in.')
             return redirect(url_for('login'))
 
-    return render_template("register.html")
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -83,21 +82,25 @@ def login():
             flash('The email does not exist in database.')
             return render_template('login.html')
 
-    return render_template("login.html")
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
     print(current_user.name)
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
+@login_required
 def download():
     return send_from_directory('static', filename='files/cheat_sheet.pdf')
 
