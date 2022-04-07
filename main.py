474f925cc0856f8f780526764fe0d3b24add5c39
krook98 <sokalski.jakub98@gmail.com>
2022-04-07 15:59:49 +0200

    Flask Flash messages

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-import werkzeug.security
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
+app.config['SECRET_KEY'] = '123456789asdfg'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 # CREATE TABLE IN DB
@@ -23,6 +33,7 @@ class User(UserMixin, db.Model):
 # Line below only required once, when creating DB.
 # db.create_all()
 
+# reload user object from the user ID stored in the session
 
 @app.route('/')
 def home():
@@ -34,28 +45,51 @@ def register():
     if request.method == 'POST':
         new_user = User(
             email=request.form.get('email'),
-            password=werkzeug.security.generate_password_hash(
+            password=generate_password_hash(
                 password=request.form.get('password'),
                 method='pbkdf2:sha256',
                 salt_length=8
             ),
             name=request.form.get('name')
         )
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('secrets', name=new_user.name))
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for('secrets'))
+        except IntegrityError:
+            flash('User already exists. Please log in.')
+            return redirect(url_for('login'))
+
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        try:
+            user = User.query.filter_by(email=email).first()
+            if check_password_hash(user.password, password):
+                login_user(user)
+                return redirect(url_for('secrets'))
+            flash('Wrong password.')
+            return render_template('login.html')
+
+        except AttributeError:
+            flash('The email does not exist in database.')
+            return render_template('login.html')
+
     return render_template("login.html")
 
 
 @app.route('/secrets')
 def secrets():
-    name = request.args.get('name')
-    return render_template("secrets.html", name=name)
+    print(current_user.name)
+    return render_template("secrets.html", name=current_user.name)
 
 
 @app.route('/logout')
